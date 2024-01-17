4ad671e287c7ee47633852a1366aea8a01748813
村山涼介 <murayamaryousuke@murayamaryousukenoMacBook-Pro.local>
2024-01-17 15:47:41 +0900

    create page

---
+++ src/app/@modal/(.)tasks/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Page({
+  params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  return <h1>My Page</h1>;
+}
